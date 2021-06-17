7c32b6293cd4fd26cc286d3dd1e78c4f5a4eff70
Maria Gabriela Suarez <maria.suarez@comafi.com.ar>
2021-06-17 11:52:43 -0300

    configuracion inicial para el microfrontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { ContactModule } from './contact/contact.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ContactModule
   ],
   providers: [],
   bootstrap: [AppComponent]
